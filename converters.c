82450dcbc9ce410be6d68246291e55c432cbcece
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-11-21 00:56:49 +0530

    Update converters.c

--- converters.c
+++ converters.c
@@ -1,8 +1,17 @@
 #include "converters.h"
 #include "globaldeclare.h"
 
+
+const char* gc_pi8arrMajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+const char* gc_pi8arrMinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
 void ColorPairToString(const ColorPair_t* f_ColorPair, char* f_pi8Buffer) {
     sprintf(f_pi8Buffer, "%s %s", \
-            MajorColorNames[f_ColorPair->enMajorColor], \
-            MinorColorNames[f_ColorPair->enMinorColor]);
+            gc_pi8arrMajorColorNames[f_ColorPair->enMajorColor], \
+            gc_pi8arrMinorColorNames[f_ColorPair->enMinorColor]);
 }
