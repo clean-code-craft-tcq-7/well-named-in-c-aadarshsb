e0513142b3dbd058ba4c33b2b41d778ffb249713
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-11-21 00:39:45 +0530

    Update converters.h

--- converters.h
+++ converters.h
@@ -3,6 +3,6 @@
 
 #include "globaldeclare.h"
 
-void ColorPairToString(const ColorPair_t* f_colorPair, char* f_buffer);
+void ColorPairToString(const ColorPair_t* f_ColorPair, char* f_pi8Buffer);
 
 #endif
