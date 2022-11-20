84551007e5ba7237c3d5b637baee49e5653db2d7
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-11-21 00:27:28 +0530

    Create converters.c

---
+++ converters.c
@@ -0,0 +1,7 @@
+#include "converters.h"
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
