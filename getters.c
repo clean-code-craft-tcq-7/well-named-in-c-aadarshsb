b1a2bf35b255adcff2a1b292613b2c04cb269573
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-11-21 01:05:27 +0530

    Update getters.c

--- getters.c
+++ getters.c
@@ -1,19 +1,21 @@
 #include "globaldeclare.h"
 #include "getters.h"
 
+extern int g_i32NumberOfMinorColors;
+
 ColorPair_t GetColorFromPairNumber(int f_i32PairNumber)
 {
     ColorPair_t ColorPair;
     int i32ZeroBasedPairNumber = f_i32PairNumber - 1;
     ColorPair.enMajorColor = \
-        (enum enumMajorColor)(i32ZeroBasedPairNumber / numberOfMinorColors);
+        (enum enumMajorColor)(i32ZeroBasedPairNumber / g_i32NumberOfMinorColors);
     ColorPair.enMinorColor = \
-        (enum enumMinorColor)(i32ZeroBasedPairNumber % numberOfMinorColors);
+        (enum enumMinorColor)(i32ZeroBasedPairNumber % g_i32NumberOfMinorColors);
     return ColorPair;
 }
 
 int GetPairNumberFromColor(const ColorPair_t* f_ColorPair) 
 {
-    return (( f_ColorPair->enMajorColor * numberOfMinorColors )+
+    return (( f_ColorPair->enMajorColor * g_i32NumberOfMinorColors ) +
            ( f_ColorPair->enMinorColor ) + 1 );
 }
