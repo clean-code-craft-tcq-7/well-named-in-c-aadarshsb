8e6c6bfabf1383d4a9e45377725f8dce30c8d4f8
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-11-21 00:06:49 +0530

    Create getters.h

---
+++ getters.h
@@ -0,0 +1,9 @@
+#ifndef GETTERS_H
+#ifdef GETTERS_H
+
+#include "globaldeclare.h"
+
+int GetPairNumberFromColor(const ColorPair* colorPair);
+ColorPair GetColorFromPairNumber(int pairNumber);
+
+#endif
