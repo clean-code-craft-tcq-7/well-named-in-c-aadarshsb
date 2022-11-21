1a744590cb448dc7c601e38125bff7a25542a1b5
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-11-22 01:28:17 +0530

    Create printcolor.h

---
+++ printcolor.h
@@ -0,0 +1,12 @@
+#ifndef PRINTCOLOR_H
+#define PRINTCOLOR_H
+
+#include "converters.h"
+#include "globaldeclare.h"
+
+extern const char* gc_pi8arrMajorColorNames[];
+extern const char* gc_pi8arrMinorColorNames[];
+
+void PrintNumberFromColour(void);
+
+#endif
