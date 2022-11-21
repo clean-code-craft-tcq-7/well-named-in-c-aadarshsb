b0037b4b018ee6f594985182e3aa1ee51d206cd5
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-11-22 02:27:00 +0530

    Update printmanual.c

--- printmanual.c
+++ printmanual.c
@@ -10,6 +10,8 @@ void PrintManual(void)
   ColorPair_t ManualColourPrint;
   char i8ColourBuffer[15];
   int i32UserResult;
+  printf("Manual for Colour Reference \n");
+  printf("Major Minor Number \n);
   for(int i32MajorIndex = 0; i32MajorIndex < 5; i32MajorIndex++)
   {
     for(int i32MinorIndex =0 ; i32MinorIndex < 5; i32MinorIndex++)
